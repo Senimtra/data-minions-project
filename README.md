c0ebdb9163bdaa7e8874f52d80ef87ccc659e1bc
Senimtra <senimtra@hotmail.com>
2022-11-22 07:14:54 +0100

    add website files + readme

---
+++ README.md
@@ -0,0 +1,38 @@
+<img src="https://bit.ly/2VnXWr2" alt="Ironhack Logo" width="100"/>
+
+# Learning with the Minions!
+
+<img src="https://imagenpng.com/wp-content/uploads/2015/07/e877470503d5c0afe487dabd77face51.png" alt="Minions" width="400"/>
+
+## E-learning portal providing jokes, scientific facts and many more
+
+* Learning and laughing with the famous Minions 
+in their 'Banana language'
+
+* Rigid scientific facts  +  hilarious jokes  = learning with fun
+
+* Everyone just loves the Minions
+
+## Web-scraping
+
+We scraped Minion character information and scientific facts.
+
+https://despicableme.fandom.com
+https://www.natgeokids.com/uk/category/discover/science/
+
+## APIs
+
+Translating into 'Banana language' and getting random jokes by using two different APIs.
+
+https://v2.jokeapi.dev/
+https://funtranslations.com/minion
+
+## Web development
+
+We created the first version of a website for our product using HTML, CSS & JavaScript.
+
+## Future improvements
+
+* Voice translation
+* Mobile app
+* More learning modules (Maths, etc.)
---
+++ script.js
@@ -0,0 +1,166 @@
+const facts = {
+	mars: [
+		'Named after the Roman God of war, Mars is the fourth planet from the sun in our solar system.',
+		'Mars is also known as the ‘Red Planet’ because, well, it’s red! This signature colour comes from the large amount of a chemical called iron oxide (or ‘rust’ as you might know it) in its rocks and soil.',
+		'Mars is the second smallest planet in the solar system after Mercury. With a diameter (distance through the middle) of 6,791 kilometres, it’s roughly half the size of Earth.',
+		'It can get pretty cold on Mars –– much colder than our own planet, since it’s further away from the sun. At the equator, temperatures can reach 20°C, but at its poles they can plummet to as low as -140°C. Brr!',
+		'Mars is home to the highest mountain in our solar system –– a volcano called Olympus Mons. Standing a whopping 24 kilometres high, it’s about three times the height of Mount Everest!',
+		'You could jump around three times higher on Mars than you can on Earth. Boing! This is because the planet’s gravity – the force that keeps us on the ground – is much weaker.',
+		'Do you like to look at the moon at night? Well, check this out –– Mars has two moons! One is called Phobos and the other Deimos.',
+		'A day on Mars is 24 hours and 37 minutes –– only a little bit longer than a day on our own planet. A year on Mars, however, is almost twice as long, lasting 687 Earth days! This is because it takes a lot longer than Earth to complete its orbit around the Sun.',
+		'Until recently, scientists believed that there was no liquid water on the surface of Mars –– only rocks, soil dust and ice. But… News flash! In 2018, they found evidence of a lake under the planet’s south polar ice cap. Exciting stuff!',
+		'Humans have not yet been to Mars, but scientists have sent spacecraft there to help them research this fascinating planet. The first spacecraft'
+	],
+	space: [
+		'One million Earths could fit inside the Sun – and the Sun is considered an average-size star.',
+		'For years it was believed that Earth was the only planet in our solar system with liquid water. More recently, NASA revealed its strongest evidence yet that there is intermittent running water on Mars, too!',
+		'Comets are leftovers from the creation of our solar system about 4.5 billion years ago – they consist of sand, ice and carbon dioxide.',
+		'You wouldn’t be able to walk on Jupiter, Saturn, Uranus or Neptune because they have no solid surface!',
+		'If you could fly a plane to Pluto, the trip would take more than 800 years!',
+		'Space junk is any human-made object orbiting Earth that no longer serves a useful purpose. Scientists estimate there are about 500,000 pieces of space junk today, including fragments from rockets and satellites, and everyday items like spanners dropped during construction of the International Space Station!',
+		'An asteroid about the size of a car enters Earth’s atmosphere roughly once a year – but it burns up before it reaches us. Phew!',
+		'The highest mountain known to man is on an asteroid called Vesta. Measuring a whopping 22km in height, it is three times as tall as Mount Everest!',
+		'There are more stars in the universe than grains of sand on all the beaches on Earth. That’s at least a billion trillion!',
+		'The sunset on Mars appears blue.'
+	],
+	human: [
+		'Your mouth produces about one litre of saliva each day!',
+		'Your brain is sometimes more active when you’re asleep than when you’re awake.',
+		'Laid end to end, an adult’s blood vessels could circle Earth’s equator four times!',
+		'The word “muscle” comes from Latin term meaning “little mouse“, which is what Ancient Romans thought flexed bicep muscles resembled.',
+		'Bodies give off a tiny amount of light that’s too weak for the eye to see.',
+		'The average person has 67 different species of bacteria in their belly button.',
+		'You lose about 4kg of skin cells every year!',
+		'Babies don’t shed tears until they’re at least one month old.',
+		'Information zooms along nerves at about 400kmph!',
+		'The human heart beats more than three billion times in an average lifespan.',
+		'Your left lung is about 10 percent smaller than your right one.',
+		'Human teeth are just as strong as shark teeth.',
+		'Scientists estimate that the nose can recognise a trillion different scents!',
+		'Humans are the only species known to blush.',
+		'Your blood makes up about eight percent of your body weight.'
+	],
+	earth: [
+		'Earth is the third planet from the sun in our solar system. Its name comes from the the old English and Germanic words meaning ‘the ground’.',
+		'Our amazing planet has been around for quite some time. By researching our planet’s rocks, scientists have calculated the Earth to be around 4.5 billion years old! ',
+		'Like all the planets, Earth orbits (travels around) around the sun. And it does so at some serious speed –– around 30 kilometres per second, in fact! It takes 365 days (one year) for the Earth to complete one full orbit. ',
+		'Have you ever wondered why we have different seasons? We’ll tell you –– it’s because the Earth is tilted 23.4 degrees on its ‘axis’, an imaginary line straight through the middle of the planet form the North Pole to the South Pole. This means that different parts of the globe are tilted towards the sun at different times of the year (or at different times during its orbit). ',
+		'Not only does Earth zoom through space, it also spins on its axis. The result? We have daytime and nighttime! As the planet rotates, the side facing the sun receives daylight and the the other is in darkness.',
+		'People often think of Earth as a gigantic sphere. But, in fact, its shape is more like a squished ball that bulges out at the equator –– an imaginary line around the middle of the planet, exactly between the North Pole and the South Pole. ',
+		'This ‘bulge’ is caused by the Earth’s spin and the effect of ‘gravity‘. Gravity is an invisible force that attracts objects towards each other. It’s this force that pulls things towards the Earth and stops us floating off into space! ',
+		'The Earth’s diameter (distance straight through the middle) measures a huge 12,800 kilometres, making it the fifth largest planet in the solar system. Jupiter, Saturn, Uranus and Neptune are even bigger!',
+		'Earth is the only planet in our solar system known to support life. This is because it has two very important things that living creatures need to survive –– lots of oxygen and lots of water! Its distance from the sun means it’s not too hot and not too cold for creatures to live on, too. ',
+		'Earth’s ‘atmosphere’ is also hugely important for sustaining life. The atmosphere is a huge blanket of gases – mostly oxygen and nitrogen – wrapped around Earth, protecting our planet from the sun’s strong rays. At the same time, the atmosphere helps keep the Earth’s temperature comfortable for living beings – and it protects us from meteors, too!'
+	],
+	stephen: [
+		'Stephen grew up in a house where education was very important. His parents were both academics who had studied at Oxford University. Dinner times were often spent in silence while the family read books!',
+		'When he was a teenager, Stephen and his friends built a computer out of old clock parts, telephone switchboards and other recycled items. His friends nicknamed him, ‘Einstein‘!',
+		'When Stephen was 17, he went to Oxford University to study physics and chemistry. He later said that he found his first year very boring! After graduating from Oxford, he went to Cambridge University to further his studies in cosmology (the science of the origin of the universe).',
+		'Sadly, when he was 21, Stephen was diagnosed with motor neurone disease (MND) and told that he only had two years to live. MND gradually affects the brain cells that communicate with the body’s muscles. Over time, sufferers struggle to walk, talk and even swallow without help.',
+		'Stephen used walking sticks and crutches after his diagnosis, but as his illness got worse he had to use an electric wheelchair to get around. He became notorious for driving it a little too fast around the streets of Cambridge and running over other students’ toes!',
+		'Stephen made many important contributions to the world of science. He developed theories about how the world began and furthered our understanding of black holes, stars and the universe. ',
+		'Stephen was always keen for his work to be accessible to everyone, not just scientists. He wrote books that explained his theories in simple terms for everyone to understand, including a children’s book. His most famous book, A Brief History of Time, sold more than 10 million copies!',
+		'In 1985, Stephen developed a life-threatening infection. He had an emergency operation that saved his life but left him unable to talk. He was given a special computer that talked for him, which he controlled by moving a muscle in his cheek – clever!',
+		'Stephen has received many awards for his work including the 1979 Albert Einstein Medal, the Order of the British Empire (Commander) in 1982 and the 1988 Wolf Prize in Physics.',
+		'Stephen is remembered as an inspiration to many people. He had an amazing mind, incredible determination and didn’t let his illness stand in his way. He defied doctors’ predictions, living for a further 55 years after his diagnosis.'
+	]
+};
+
+const subjects = ['mars', 'space', 'human', 'earth', 'stephen'];
+
+const characters = [
+	"Bob is a Minion who is more childish and immature than most of the other Minions. He is described as a 'Little Brother' who finds love in anything and everything, including a rat (who he named Poochy) he found in a sewer in the Minions film. He also enjoys bedtime stories and playing with his favorite stuffed teddy bear, Tim. However, in the audition scene in Despicable Me 2 (having grown since the time of Minions), he became more plumper which made him look more like the short and plump Minions with spiky hair, just no hair. Is also shown more mature, albeit still playful, as well as more of a goofball like all the other Minions.",
+	'Stuart is playful, friendly, intelligent and funny. He is skillful at video games like his friend Dave. He can also be the most sincere and innocent out of all the minions. He also seems to have a rebellious and obstreperous attitude. He is considered to be a slacker among the other Minions, preferring to chill out and play the ukulele than listen to the rules. Also, he was said to be always hungry in Minions, such as when he attempted to eat Kevin and Bob when he was thinking of them as bananas, and even tried to sing a yellow fire hydrant in New York, thinking that it was a female minion.',
+	"Dave is an intelligent minion who is kind, caring, and funny, but sometimes accident-prone. He is skillful at video games like his friend Stuart, shown when the two are playing on a gaming console. Dave likes ice cream, as shown when Gru calls him and Stuart while they are having an Ice Cream Party, he's still eating Ice Cream before he goes to Gru. He is always very excited as seen when he fired his Rocket Launcher, his signature weapon. He is also good at using weapons and making cupcakes, among other foods. He also has some good fighting skills, it is shown when he swing around the pole to kick an evil minion and it fell on the ground with the others.",
+	'Kevin loves to make fun of and tease people or Minions, shown when he made fun of Jerry and teased him for being a coward. He likes playing golf polo and cricket. Kevin has a sense of humor, shown when he thought it was hilarious to see Tom turn into a mutant purple minion, but then immediately showed a cautious side when he realized that he was next to be a victim. In the film Minions, he is the protagonist and leader of the trio in search of a new master. He truly cares about the well-being of the Minion tribe (which is dependent on them having a proper master).'
+];
+
+const inputSpeak = document.querySelector('#inputSpeak');
+const btnSpeak = document.querySelector('#btnSpeak');
+const h1Speak = document.querySelector('#h1Speak');
+const btnJokes = document.querySelector('#btnJokes');
+const h1Jokes = document.querySelector('#h1Jokes');
+const btnJokes2 = document.querySelector('#btnJokes2');
+const btnFact = document.querySelector('#btnFact');
+const h1Fact = document.querySelector('#h1Fact');
+const btnFact2 = document.querySelector('#btnFact2');
+const h1Character = document.querySelector('#h1Character');
+const btnCharacter = document.querySelector('#btnCharacter');
+const imgbob = document.querySelector('#bob');
+const imgstuart = document.querySelector('#stuart');
+const imgdave = document.querySelector('#dave');
+const imgkevin = document.querySelector('#kevin');
+
+const listcharacters = [imgbob, imgstuart, imgdave, imgkevin];
+
+let urlJokes =
+	'https://v2.jokeapi.dev/joke/Miscellaneous?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
+let urlbanana = 'https://api.funtranslations.com/translate/minion.json';
+let url = 'https://api.funtranslations.com/translate/minion.json';
+
+let englishJoke;
+let englishFact;
+let index = 0;
+
+const tellJokes = () => {
+	fetch(urlJokes)
+		.then(data => data.json())
+		.then(data => {
+			englishJoke = data.joke;
+			jokeBanana(englishJoke);
+		});
+};
+
+const jokeBanana = jokeEnglish => {
+	const newUrl = `${urlbanana}?text=${jokeEnglish}`;
+	fetch(newUrl, {
+		headers: {}
+	})
+		.then(data => data.json())
+		.then(data => (h1Jokes.innerHTML = data.contents.translated));
+};
+
+const tellFact = () => {
+	const subject = subjects[Math.floor(Math.random() * subjects.length)];
+	const marsFacts = facts[subject];
+	englishFact = `${subject} ${marsFacts[Math.floor(Math.random() * marsFacts.length)]}`;
+	factBanana(englishFact);
+};
+
+const factBanana = factEnglish => {
+	const newUrl = `${url}?text=${factEnglish}`;
+	fetch(newUrl, {
+		headers: {}
+	})
+		.then(data => data.json())
+		.then(data => (h1Fact.innerHTML = data.contents.translated));
+};
+
+const Translate = () => {
+	let inputValue = inputSpeak.value;
+	newUrl = `${url}?text=${inputValue}`;
+	fetch(newUrl, {
+		headers: {}
+	})
+		.then(data => data.json())
+		.then(data => (h1Speak.innerHTML = data.contents.translated));
+};
+
+const selectCharacter = () => {
+	h1Character.innerHTML = characters[index];
+	listcharacters[index].classList.add('show');
+	listcharacters[index].classList.remove('hidden');
+	if (listcharacters[index - 1]) listcharacters[index - 1].classList.add('hidden');
+	if (index === characters.length - 1) index = 0;
+	else index++;
+};
+
+const TranslateEnglish = () => (h1Jokes.innerHTML = englishJoke);
+const factEnglish = () => (h1Fact.innerHTML = englishFact);
+
+btnSpeak.addEventListener('click', Translate);
+btnJokes.addEventListener('click', tellJokes);
+btnJokes2.addEventListener('click', TranslateEnglish);
+btnFact.addEventListener('click', tellFact);
+btnFact2.addEventListener('click', factEnglish);
+btnCharacter.addEventListener('click', selectCharacter);
